829c53d557d9baca8e818de6c357e8a83bf3f868
Narendra3383 <69565966+Narendra3383@users.noreply.github.com>
2021-03-19 16:30:12 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myList = new List<string>();
+            myList.Add("1.siva");
+            myList.Add("2.Kiran");
+            myList.Add("3.Ashok");
+            myList.Add("4.narendra");
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+            myList.Sort();
+            Console.WriteLine("\n After sorting");
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+            myList.Remove("narendra");
+            Console.WriteLine("\n Removing of narendra");
+            foreach (string s in myList)
+                Console.WriteLine(s.ToString() + " ");
+            myList.Insert(2, "Thulasi");
+            Console.WriteLine("\n Inserting of Thulasi at index position 2");
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+        }
+    }
+}
